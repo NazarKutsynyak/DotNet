9d34be46ab4f571de632d7cc72afe44e3814e1f5
Nazar Kutsynyak <44039482+NazarKutsynyak@users.noreply.github.com>
2018-10-18 13:26:01 +0300

    Using OOP

--- Desktop/Test/Test1/PLLUGTest/Program.cs
+++ Desktop/Test/Test1/PLLUGTest/Program.cs
@@ -1,55 +1,62 @@
 ﻿using System;
+using static System.Console;
+using static System.Math;
+using static System.Convert;
 
 
-namespace PLLUGTest
+namespace Test3_OOP
 {
-    class Program
+    class Hole
     {
-        static int FormOfHole = 0, FormOfObject = 0;
-        static double RectangleWidth = 0, RectangleHeight = 0, RingDiameter = 0;
-        static double CubeWidth = 0, CubeHeight = 0, CubeDepth = 0;
-        static double BallDiameter = 0;
-        static double CylinderDiameter = 0, CylinderHeight = 0;
+        public int Form;
+        public double RingDiameter, RectangleWidth, RectangleHeight;
 
-        static void SetHole()
+        public void Set()
         {
-            do
+            WriteLine("Привіт! Введи будьласка форму отвору");
+            WriteLine("Якщо прямокутник, введи 1");
+            WriteLine("Якщо круг, введи 2");
+
+            Form = ToInt32(ReadLine());
+
+            if (Form == 1)
             {
-                Console.WriteLine("Привіт! Введи будьласка форму отвору");
-                Console.WriteLine("Якщо прямокутник, введи 1");
-                Console.WriteLine("Якщо круг, введи 2");
+                WriteLine("Ви вибрали прямокутник");
 
-                FormOfHole = Convert.ToInt32(Console.ReadLine());
+                WriteLine("Введіть ширину (формат х,хх):");
+                RectangleWidth = ToDouble(ReadLine());
 
-                if (FormOfHole == 1)
-                {
-                    Console.WriteLine("Ви вибрали прямокутник");
+                WriteLine("Введіть висоту (формат х,хх): ");
+                RectangleHeight = ToDouble(ReadLine());
+            }
+            else if (Form == 2)
+            {
+                WriteLine("Ви вибрали круг");
 
-                    Console.WriteLine("Введіть ширину (формат х,хх):");
-                    RectangleWidth = Convert.ToDouble(Console.ReadLine());
+                WriteLine("Введіть діаметр (формат х,хх): ");
+                RingDiameter = ToDouble(ReadLine());
+            }
 
-                    Console.WriteLine("Введіть висоту (формат х,хх): ");
-                    RectangleHeight = Convert.ToDouble(Console.ReadLine());
-                }
-                else if (FormOfHole == 2)
-                {
-                    Console.WriteLine("Ви вибрали круг");
-
-                    Console.WriteLine("Введіть діаметр (формат х,хх): ");
-                    RingDiameter = Convert.ToDouble(Console.ReadLine());
-                }
-            } while (FormOfHole < 1 && FormOfHole > 2);
         }
 
-        static void SetObject()
+
+    }
+    class Object
+    {
+        public int Form;
+        public double BallDiameter;
+        public double CylinderDiameter, CylinderHeight;
+        public double CubeWidth, CubeHeight, CubeDepth;
+
+        public void Set()
         {
-            Console.WriteLine("Виберіть форму предмету, який хочете просунути через отвір");
-            Console.WriteLine("Якщо куб введіть 1\nЯкщо куля введіть 2");
-            Console.WriteLine("Якщо циліндр введіть 3");
+            WriteLine("Виберіть форму предмету, який хочете просунути через отвір");
+            WriteLine("Якщо куб введіть 1\nЯкщо куля введіть 2");
+            WriteLine("Якщо циліндр введіть 3");
 
-            FormOfObject = Convert.ToInt32(Console.ReadLine());
+            Form = ToInt32(ReadLine());
 
-            if (FormOfObject == 1)
+            if (Form == 1)
             {
                 Console.WriteLine("Ви вибрали куб");
 
@@ -62,120 +69,135 @@ namespace PLLUGTest
                 Console.WriteLine("Введіть глибину:");
                 CubeDepth = Convert.ToDouble(Console.ReadLine());
             }
-            else if (FormOfObject == 2)
+            else if (Form == 2)
             {
-                Console.WriteLine("Ви вибрали кулю");
+                WriteLine("Ви вибрали кулю");
 
-                Console.WriteLine("Введіть діаметр кулі:");
-                BallDiameter = Convert.ToDouble(Console.ReadLine());
+                WriteLine("Введіть діаметр кулі:");
+                BallDiameter = ToDouble(ReadLine());
             }
-            else if (FormOfObject == 3)
+            else if (Form == 3)
             {
-                Console.WriteLine("Ви вибрали цилінд");
+                WriteLine("Ви вибрали цилінд");
 
-                Console.WriteLine("Введіть діаметр:");
-                CylinderDiameter = Convert.ToDouble(Console.ReadLine());
+                WriteLine("Введіть діаметр:");
+                CylinderDiameter = ToDouble(ReadLine());
 
-                Console.WriteLine("Введіть висоту:");
-                CylinderHeight = Convert.ToDouble(Console.ReadLine());
+                WriteLine("Введіть висоту:");
+                CylinderHeight = ToDouble(ReadLine());
             }
         }
 
-        static void VerificationRectangle() 
+        public void VerificationBall(int FormOfHole, double a, double b)
         {
-            if (FormOfObject == 1)
+            if (FormOfHole == 1)
             {
-                if (RectangleWidth > CubeWidth && RectangleHeight > CubeHeight)
-                    Console.WriteLine("Ви можете просунути цей куб.");
+                if (a > BallDiameter && b > BallDiameter)
+                    WriteLine("Ви можете просунути цю кулю");
 
-                else if (RectangleWidth > CubeHeight && RectangleHeight > CubeWidth)
-                    Console.WriteLine("Ви можете просунути цей куб, але вам потрібно його повернути");
-
-
-                else if (RectangleWidth > CubeWidth && RectangleHeight > CubeDepth)
-                    Console.WriteLine("Ви можете просунути цей куб, але вам потрібно його повернути");
-
-                else if (RectangleWidth > CubeDepth && RectangleHeight > CubeWidth)
-                    Console.WriteLine("Ви можете просунути цей куб, але вам потрібно його повернути");
-
-
-                else if (RectangleWidth > CubeHeight && RectangleHeight > CubeDepth)
-                    Console.WriteLine("Ви можете просунути цей куб, але вам потрібно його повернути");
-
-                else if (RectangleWidth > CubeDepth && RectangleHeight > CubeHeight)
-                    Console.WriteLine("Ви можете просунути цей куб, але вам потрібно його повернути");
-
-
-                else Console.WriteLine("Ви не зможете просути цей куб");
+                else WriteLine("Ви не зможете пронути цю кулю");
             }
-
-            else if(FormOfObject == 2)
+            if (FormOfHole == 2)
             {
-                if (RectangleWidth > BallDiameter && RectangleHeight > BallDiameter)
-                    Console.WriteLine("Ви можете просунути цю кулю");
-
-                else Console.WriteLine("Ви не зможете пронути цю кулю");
+                if (a > BallDiameter)
+                    WriteLine("Ця куля пролізе в отвір");
+                else WriteLine("Ця куля не пролізе в отвір");
             }
+        }
 
-            else if(FormOfObject == 3)
+        public void VerificationCylinder(int FormOfHole, double a, double b)
+        {
+            if (FormOfHole == 1)
             {
-                if (RectangleWidth > CylinderDiameter && RectangleHeight > CylinderHeight)
+                if (a > CylinderDiameter && b > CylinderHeight)
                     Console.WriteLine("Ви можете просунути цей циліндр");
-                else if (RectangleWidth > CylinderHeight && RectangleHeight > CylinderDiameter)
+                else if (a > CylinderHeight && b > CylinderDiameter)
                     Console.WriteLine("Ви можете просунути цей циліндр, але вам потрібно його повернути");
-                else if (RectangleWidth > CylinderDiameter && RectangleHeight > CylinderDiameter)
+                else if (a > CylinderDiameter && b > CylinderDiameter)
                     Console.WriteLine("Ви можете просунути цей циліндр, але вам потрібно його повернути");
                 else Console.WriteLine("Ви не зможете просунути цей циліндр");
             }
-            
-        }
-
-        static void VerificationRing()
-        {
-            if (FormOfObject == 1)
-            {
-                double CubeHypotenuseA = Math.Sqrt((CubeWidth * CubeWidth) + (CubeHeight * CubeHeight));
-                double CubeHypotenuseB = Math.Sqrt((CubeWidth * CubeWidth) + (CubeDepth * CubeDepth));
-                double CubeHypotenuseC = Math.Sqrt((CubeHeight * CubeHeight) + (CubeDepth * CubeDepth));
-
-                if (RingDiameter > CubeHypotenuseA)
-                    Console.WriteLine("Цей прямокутник пролізе в отвір");
-                else if (RingDiameter > CubeHypotenuseB)
-                    Console.WriteLine("Цей прямокутник пролізе в отвір, але вам потрібно його повернути");
-                else if (RingDiameter > CubeHypotenuseC)
-                    Console.WriteLine("Цей прямокутник пролізе в отвір, але вам потрібно його повернути");
-                else Console.WriteLine("Цей прямокутник не пролізе в отвір");
-            }
-            else if (FormOfObject == 2)
-            {
-                if (RingDiameter > BallDiameter)
-                    Console.WriteLine("Ця куля пролізе в отвір");
-                else Console.WriteLine("Ця куля не пролізе в отвір");
-            }
-            else if(FormOfObject == 3)
+            if (FormOfHole == 2)
             {
                 double CylinderHypotenuse = Math.Sqrt((CylinderDiameter * CylinderDiameter) + (CylinderHeight * CylinderHeight));
 
-                if (RingDiameter > CylinderHypotenuse)
+                if (a > CylinderHypotenuse)
                     Console.WriteLine("Цей циліндр пролізе в отвір");
-                else if (RingDiameter > CylinderDiameter)
+                else if (a > CylinderDiameter)
                     Console.WriteLine("Цей циліндр пролізе в отвір");
                 else Console.WriteLine("Цей циліндр не пролізе в отвір");
             }
         }
 
-        static void Main(string[] args)
+        public void VerificationCube(int FormOfHole, double a, double b)
         {
-            Console.OutputEncoding = System.Text.Encoding.Default;
-            //SetHole();
-            FormOfHole = 2;
-            RingDiameter = 100;
-            //SetObject();
-            FormOfObject = 3;
-            CylinderDiameter = 100;
-            CylinderHeight = 100;
-            if (FormOfHole == 1) VerificationRectangle();
-            else VerificationRing();
+            if (FormOfHole == 1)
+            {
+                if (a > CubeWidth && b > CubeHeight)
+                    Console.WriteLine("Ви можете просунути цей куб.");
+
+                else if (a > CubeHeight && b > CubeWidth)
+                    Console.WriteLine("Ви можете просунути цей куб, але вам потрібно його повернути");
+
+
+                else if (a > CubeWidth && b > CubeDepth)
+                    Console.WriteLine("Ви можете просунути цей куб, але вам потрібно його повернути");
+
+                else if (a > CubeDepth && b > CubeWidth)
+                    Console.WriteLine("Ви можете просунути цей куб, але вам потрібно його повернути");
+
+
+                else if (a > CubeHeight && b > CubeDepth)
+                    Console.WriteLine("Ви можете просунути цей куб, але вам потрібно його повернути");
+
+                else if (a > CubeDepth && b > CubeHeight)
+                    Console.WriteLine("Ви можете просунути цей куб, але вам потрібно його повернути");
+
+
+                else Console.WriteLine("Ви не зможете просути цей куб");
+            }
+            if (FormOfHole == 2)
+            {
+                double CubeHypotenuseA = Math.Sqrt((CubeWidth * CubeWidth) + (CubeHeight * CubeHeight));
+                double CubeHypotenuseB = Math.Sqrt((CubeWidth * CubeWidth) + (CubeDepth * CubeDepth));
+                double CubeHypotenuseC = Math.Sqrt((CubeHeight * CubeHeight) + (CubeDepth * CubeDepth));
+
+                if (a > CubeHypotenuseA)
+                    Console.WriteLine("Цей прямокутник пролізе в отвір");
+                else if (a > CubeHypotenuseB)
+                    Console.WriteLine("Цей прямокутник пролізе в отвір, але вам потрібно його повернути");
+                else if (a > CubeHypotenuseC)
+                    Console.WriteLine("Цей прямокутник пролізе в отвір, але вам потрібно його повернути");
+                else Console.WriteLine("Цей прямокутник не пролізе в отвір");
+            }
+        }
+
+
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Hole hole = new Hole();
+            Object obj = new Object();
+
+            hole.Set();
+            obj.Set();
+
+            if (hole.Form == 1)
+            {
+                if (obj.Form == 1) obj.VerificationCube(hole.Form, hole.RectangleWidth, hole.RectangleHeight);
+                if (obj.Form == 2) obj.VerificationBall(hole.Form, hole.RectangleWidth, hole.RectangleHeight);
+                if (obj.Form == 3) obj.VerificationCylinder(hole.Form, hole.RectangleWidth, hole.RectangleHeight);
+            }
+            if (hole.Form == 2)
+            {
+                if (obj.Form == 1) obj.VerificationCube(hole.Form, hole.RingDiameter, 0);
+                if (obj.Form == 2) obj.VerificationBall(hole.Form, hole.RingDiameter, 0);
+                if (obj.Form == 3) obj.VerificationCylinder(hole.Form, hole.RingDiameter, 0);
+            }
+
         }
     }
 }

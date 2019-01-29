d05f598fe798cb9091463f1bffaecdd32b3b71c6
Nazar Kutsynyak <44039482+NazarKutsynyak@users.noreply.github.com>
2019-01-29 23:03:54 +0200

    Some

---
+++ Desktop/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your name: ");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}!!!");
+            Console.Write("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
